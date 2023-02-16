96357561f56f74dcba693ce9ca42a0434305af08
Yana Koroliuk <yana.koroyuk.097@gmail.com>
2023-02-16 15:49:20 +0200

    Update 2 point with struct

--- lab1.c
+++ lab1.c
@@ -2,29 +2,33 @@
 #include <math.h>
 
 float func4(float x);
-float func2(float x, float i);
+struct Str func2(float x, float i);
 
-
-float result = 1;
+struct Str {
+    float fi;
+    float sum;
+};
 
 int main() {
-    printf("%f\n", func4(2));
+    printf("Iteration result: %f\n", func4(2));
 
-    func2(2, 5);
-    printf("%f \n", result);
+    struct Str res2 = func2(2, 5);
+    printf("Recursion_2 result: %f\n", res2.sum);
 
-    printf("%f", cos(2));
+    printf("Correct result: %f", cos(2));
     return 0;
 
 }
 
-float func2(float x, float i) {
+struct Str func2(float x, float i) {
+    static struct Str result = {1, 1};
     if (i == 0) {
-        return 1;
+        return result;
     }
-    float fi = -func2(x, i-1)*x*x/(4*i*i-2*i);
-    result += fi;
-    return fi;
+    float fi = -func2(x, i-1).fi*x*x/(4*i*i-2*i);
+    result.fi = fi;
+    result.sum += fi;
+    return result;
 }
 
 float func4(float x) {
